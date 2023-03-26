417ed25b9964e2c8f6f775b28690f8d1e20bc72b
honkling <untitledgeese@gmail.com>
2023-03-26 12:43:02 -0500

    Revert "Update UwULogger.kt"
    This reverts commit 47380588e859e48db085fa7d330eae9a57e2bbe3.
    This commit introduces an infinite loop.

--- src/main/kotlin/me/honkling/uwulogs/lib/UwULogger.kt
+++ src/main/kotlin/me/honkling/uwulogs/lib/UwULogger.kt
@@ -1,6 +1,8 @@
 package me.honkling.uwulogs.lib
 
 import org.bukkit.plugin.Plugin
+import java.io.OutputStream
+import java.io.PrintStream
 import java.util.logging.Level
 import java.util.logging.LogRecord
 import java.util.logging.Logger
@@ -27,12 +29,12 @@ class UwULogger(
             log.message = log.message.replace("{$i}", v.toString())
         }
 
-        log(Level.INFO, log.message)
+        println(log.message)
 
         if (log.thrown != null) {
             log.thrown.printStackTrace(object : PrintStream(System.err) {
                 override fun println(x: Any?) {
-                    log(Level.SEVERE, uwuify(x.toString()))
+                    println(uwuify(x.toString()))
                 }
             })
         }
