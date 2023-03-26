e39b273ace0b51110144ec2bd621f7ba4ffd2464
cevxl <119167508+cevxl@users.noreply.github.com>
2023-03-26 07:42:49 +0200

    Update StringUtils.kt

--- src/main/kotlin/me/honkling/uwulogs/lib/StringUtils.kt
+++ src/main/kotlin/me/honkling/uwulogs/lib/StringUtils.kt
@@ -2,5 +2,5 @@ package me.honkling.uwulogs.lib
 
 // Thanks https://github.com/UwUAroze/UwUCore/blob/master/src/main/java/me/aroze/uwucore/util/ChatUtils.kt
 fun String.replaceCaseInsensitive(text: String, replacement: String): String {
-    return this.replace(Regex("(?i)$text"), replacement)
+    return this.replace(Regex("${Regex.escape(text)}", RegexOption.IGNORE_CASE), replacement)
 }
